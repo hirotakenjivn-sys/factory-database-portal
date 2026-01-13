3d7f651c1a6913daf55b158b5584f09193ff5fbd
Hirota Kenji <hirotakenjivn@gmail.com>
2026-01-13 14:52:34 +0700

    process hilight 3

--- frontend/src/views/master/ProductDetailView.vue
+++ frontend/src/views/master/ProductDetailView.vue
@@ -143,7 +143,7 @@
                   <!-- View Mode -->
                   <td>{{ process.process_no }}</td>
                   <td>{{ process.process_name }}</td>
-                  <td :class="{ 'highlight-zero-cycletime': process.rough_cycletime === 0 }">{{ process.rough_cycletime || '-' }}</td>
+                  <td :class="{ 'highlight-zero-cycletime': Number(process.rough_cycletime) === 0 }">{{ process.rough_cycletime || '-' }}</td>
                   <td>{{ process.production_limit ? process.production_limit.toLocaleString() : '-' }}</td>
                   <td>{{ process.setup_time || '-' }}</td>
                   <td>
